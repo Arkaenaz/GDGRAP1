c8c40f9e44745722cdc7b1ab8c17036d9c1118b8
Arkaenaz <flooper2234@gmail.com>
2023-05-19 10:45:33 +0800

    not a pentagon anymore

--- Pentagon/Pentagon/Pentagon.cpp
+++ Pentagon/Pentagon/Pentagon.cpp
@@ -1,5 +1,9 @@
+#include "glad/glad.h"
 #include <GLFW/glfw3.h>
 #include "cmath"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
 
 int main(void)
 {
@@ -19,6 +23,78 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    gladLoadGL();
+
+    std::string path = "3D/bunny.obj";
+    std::vector<tinyobj::shape_t> shape;
+    std::vector<tinyobj::material_t> material;
+    std::string warning, error;
+    
+    tinyobj::attrib_t attributes;
+
+    bool success = tinyobj::LoadObj(
+        &attributes,
+        &shape,
+        &material,
+        &warning,
+        &error,
+        path.c_str()
+    );
+
+    std::vector<GLuint> mesh_indices;
+    for (int i = 0; i < shape[0].mesh.indices.size(); i++) {
+        mesh_indices.push_back(
+            shape[0].mesh.indices[i].vertex_index
+        );
+    }
+
+    GLfloat vertices[]{
+        0.f, 0.5f, 0.f,
+        -0.5f, -0.5f, 0.f,
+        0.5f, -0.5f, 0.f
+    };
+
+    GLuint indices[]{
+        0,1,2
+    };
+
+    GLuint VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    // working with this VAO
+    glBindVertexArray(VAO);
+    // working with this VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        sizeof(GLfloat) * attributes.vertices.size(),
+        &attributes.vertices[0],
+        GL_STATIC_DRAW
+    );
+
+    glVertexAttribPointer(
+        0,
+        3, //XYZ
+        GL_FLOAT,
+        GL_FALSE,
+        3 * sizeof(GLfloat),
+        (void*)0
+    );
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(
+        GL_ELEMENT_ARRAY_BUFFER,
+        sizeof(GLuint) * mesh_indices.size(),
+        mesh_indices.data(),
+        GL_STATIC_DRAW
+    );
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -26,24 +102,15 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Triangle Version
-        /*for (float i = 0; i < 5; i++) {
-            float angle1 = (0 + 72 * i) * 3.14 / 180;
-            float angle2 = (72 + 72 * i)* 3.14 / 180;
-            glBegin(GL_TRIANGLES);
-            glVertex2f(0.f, 0.f);
-            glVertex2f(0.3f * cos(angle1), 0.3f * sin(angle1));
-            glVertex2f(0.3f * cos(angle2), 0.3f * sin(angle2));
-            glEnd();
-        }*/
-
-        glBegin(GL_POLYGON);
-        for (float i = 0; i < 5; i++) {
-            float angle1 = (0 + 72 * i) * 3.14 / 180;
-            float angle2 = (72 + 72 * i) * 3.14 / 180;
-            glVertex2f(0.3f * cos(angle1), 0.3f * sin(angle1));
-        }
-        glEnd();
+        glBindVertexArray(VAO);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(
+            GL_TRIANGLES,
+            mesh_indices.size(),
+            GL_UNSIGNED_INT,
+            0
+        );
+        
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -51,6 +118,10 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
