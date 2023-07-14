d2bbb8334738081a750f757d4e76acf02494a6c7
Arkaenaz <flooper2234@gmail.com>
2023-07-14 09:52:44 +0800

    a

--- Week 10/Main.cpp
+++ Week 10/Main.cpp
@@ -293,7 +293,12 @@ int main(void)
     glBindVertexArray(skyboxVAO);
     glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void*)0)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void*)0);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, skyboxEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(skyboxIndices), &skyboxIndices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
 
 
     std::vector<GLuint> mesh_indices;
