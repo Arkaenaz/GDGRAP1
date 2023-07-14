2833f4a8342cd42dbb2934fd1eb36bfdfa6b3caf
Arkaenaz <flooper2234@gmail.com>
2023-07-14 09:48:09 +0800

    yes

--- Week 10/Main.cpp
+++ Week 10/Main.cpp
@@ -243,6 +243,59 @@ int main(void)
         path.c_str()
     );
 
+    /*
+  7--------6
+ /|       /|
+4--------5 |
+| |      | |
+| 3------|-2
+|/       |/
+0--------1
+*/
+//Vertices for the cube
+    float skyboxVertices[]{
+        -1.f, -1.f, 1.f, //0
+        1.f, -1.f, 1.f,  //1
+        1.f, -1.f, -1.f, //2
+        -1.f, -1.f, -1.f,//3
+        -1.f, 1.f, 1.f,  //4
+        1.f, 1.f, 1.f,   //5
+        1.f, 1.f, -1.f,  //6
+        -1.f, 1.f, -1.f  //7
+    };
+
+    //Skybox Indices
+    unsigned int skyboxIndices[]{
+        1,2,6,
+        6,5,1,
+
+        0,4,7,
+        7,3,0,
+
+        4,5,6,
+        6,7,4,
+
+        0,3,2,
+        2,1,0,
+
+        0,1,5,
+        5,4,0,
+
+        3,7,6,
+        6,2,3
+    };
+
+    unsigned int skyboxVAO, skyboxVBO, skyboxEBO;
+    glGenVertexArrays(1, &skyboxVAO);
+    glGenBuffers(1, &skyboxVBO);
+    glGenBuffers(1, &skyboxEBO);
+
+    glBindVertexArray(skyboxVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void*)0)
+
+
     std::vector<GLuint> mesh_indices;
     for (int i = 0; i < shape[0].mesh.indices.size(); i++) {
         mesh_indices.push_back(
